57ce0b522250c6e629bbd504a74a2240f420cca1
Dang Ton <dton@fastenal.com>
2019-04-02 16:23:53 -0500

    Init Commit

---
+++ js/countdown.js
@@ -0,0 +1,48 @@
+(function($){
+// Set the date we're counting down to
+var countDownDate = new Date("Jul 28, 2019 17:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+var fullStory = document.getElementById('full-story');    
+fullStory.onclick = function() {
+    var testV = 1;
+    var pass1 = prompt('Please Enter Your Password',' ');
+    while (testV < 3) {
+        if (pass1.toLowerCase() == "tinder") {
+          $('#portfolioModal3').modal();    
+          break;
+        } 
+    testV+=1;
+    var pass1 = 
+    prompt('Access Denied - Password Incorrect, Please Try Again.','Password');
+    }
+    if (pass1.toLowerCase()!="password" & testV ==3) 
+    return " ";
+}     
+    
+})(jQuery);
